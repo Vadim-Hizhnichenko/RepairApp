cc40a637773177b724f921167ccff3353ab1f246
Vadim Hizhnichenko <yankeevadim@gmail.com>
2022-06-11 18:48:18 +0300

    add structure file (c and s)

--- repair-app/src/app/app.module.ts
+++ repair-app/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RepairComponent } from './components/repair/repair.component';
+import { ShowRepairComponent } from './components/show-repair/show-repair.component';
+import { AddRepairComponent } from './components/add-repair/add-repair.component';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RepairApiService } from './services/repair-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepairComponent,
+    ShowRepairComponent,
+    AddRepairComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [RepairApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ repair-app/src/app/components/show-repair/show-repair.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-repair',
+  templateUrl: './show-repair.component.html',
+  styleUrls: ['./show-repair.component.css']
+})
+export class ShowRepairComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
